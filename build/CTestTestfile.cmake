0aebe31a409f4371ae68775eb254688cc15744d4
Seixas <mateus_seixas@hotmail.com.br>
2021-08-03 17:41:01 -0300

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/senai/vertbot_ws/src
+# Build directory: /home/senai/vertbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bir_vertbot")
+subdirs("joystick_control")
---
+++ build/joystick_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/senai/vertbot_ws/src/joystick_control
+# Build directory: /home/senai/vertbot_ws/build/joystick_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
